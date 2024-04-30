e71c9d40df2a7d3ff9b0c6e4357a26b09c87aacb
Alex Lindholm <alexlindholm02@gmail.com>
2024-04-30 09:49:21 +0300

    tyhjä poistettu

--- src/components/Placesearch.jsx
+++ src/components/Placesearch.jsx
@@ -34,7 +34,7 @@ const PlaceSearch = ({ setUrl }) => {
     }
     //haku rakenne
     return (
-        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <div style={{ alignItems: 'center', justifyContent: 'center' }}>
             <Autocomplete
                 value={searchTerm}
                 options={options}
